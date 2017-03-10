2836aa68b521227afe9e4a97d662a7674d58acfe
Alan Thomson <alan@projectmccoll.co.uk>
2017-03-10 09:59:20 +0000

    remove creds

--- app/lib/config.js
+++
@@ -1,7 +0,0 @@
-// Application credentials from the Azure Management Portal.
-module.exports = {
-  clientId: 'e91b6ce4-702e-4d91-8fd0-a0a216854b24',
-  clientSecret: '3e15XvO5fwW6qXCHOhXRjySem7Uo+sedShUUSw5jLU8=',
-  tokenEndpoint: 'https://login.windows.net/international-alert.org/oauth2/token',
-  groupId: '375abe38-5b56-4deb-b725-02ce52b2a339'
-};
