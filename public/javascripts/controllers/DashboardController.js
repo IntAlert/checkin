9d66d7875f09c8524c99c9761a2ae4e42fedfcfb
Alan Thomson <alan@projectmccoll.co.uk>
2017-04-21 11:29:10 +0200

    fix race condition

--- public/javascripts/controllers/DashboardController.js
+++ public/javascripts/controllers/DashboardController.js
@@ -1,8 +1,12 @@
 app.controller('DashboardContoller', function ($scope, $window, $document, $location, $mdDialog, $interval, UsersService, EntriesService) {
 
 
+	// settings
+	var UserPollInterval = 1000 * 60 * 60;
+	var EntryStatusPollInterval = 3000;
 
-	var keepPolling = true;
+
+	var keepPollingEntryStatuses = false;
 
 	// User-generated data
 	$scope.data = {
@@ -11,23 +15,29 @@ app.controller('DashboardContoller', function ($scope, $window, $document, $loca
 
 
 	// get all users
-
 	function getUsers() {
-		UsersService.getAll()
+		console.log('getting users')
+		return UsersService.getAll()
 		.then(users => {
 			$scope.data.users = users
+			return users
 		})	
 	}
 
-	getUsers()
+	// get users for the first time
+	// and then allow pollin statuses to start
+	getUsers().then(users => {
+		keepPollingEntryStatuses = true;
+	})
 
-	$interval(getUsers, 1000 * 60 * 60)
-	
+	// refresh users every hour
+	$interval(getUsers, UserPollInterval)
 
 
+	// confirm toggle in/out
 	$scope.showConfirm = function(ev, user) {
 
-		keepPolling = false;
+		keepPollingEntryStatuses = false;
 
 		if (user.in) {
 			// They are signing out
@@ -50,32 +60,43 @@ app.controller('DashboardContoller', function ($scope, $window, $document, $loca
 
 	    $mdDialog.show(confirm).then(function() {
 	    	// confirmed
-	    	keepPolling = true;
-	    	return toggle(user)
+	    	return toggle(user).then( () => {
+	    		// allow polling to recommence once the change has been made
+	    		// this prevents a race condition between the local data and poll results
+	    		// which can cause a flicker between in/out
+	    		keepPollingEntryStatuses = true;	
+	    	})
 	    }, function() {
 	    	// cancelled
-	    	keepPolling = true;
+	    	keepPollingEntryStatuses = true;
 	    });
 	  };
 
 	var toggle = function(user){
-		if (user.in) return EntriesService.signOut(user.id)
-		else return EntriesService.signIn(user.id)
+
+		// toggle the local copy
+		user.in = ! user.in;
+
+		// now call Entries service based on NEW state
+		if (user.in) return EntriesService.signIn(user.id)
+		else return EntriesService.signOut(user.id)
 	}
 
 
 	var pollEntryStatus = function() {
 		EntriesService.getStatuses().then(statuses => {
+
+			// append in/out status to the users array
 			for (var i = $scope.data.users.length - 1; i >= 0; i--) {
 
-				var statusToday = statuses.filter(function(s){
+				var statusToday = statuses.find(function(s){
 					return s.ad_id == $scope.data.users[i].id
 				})
 
-				if (statusToday.length) {
+				if (statusToday !== undefined) {
 					// update status
-					$scope.data.users[i].in = statusToday[0].in
-					$scope.data.users[i].updatedAt = statusToday[0].updatedAt
+					$scope.data.users[i].in = statusToday.in
+					$scope.data.users[i].updatedAt = statusToday.updatedAt
 				} else {
 					// no status today
 					$scope.data.users[i].in = false;
@@ -85,8 +106,9 @@ app.controller('DashboardContoller', function ($scope, $window, $document, $loca
 		})	
 	}
 
-	setInterval(function(){
-		if (keepPolling) pollEntryStatus()
-	}, 1000)
+	// check entry status every second
+	$interval(function(){
+		if (keepPollingEntryStatuses) pollEntryStatus()
+	}, EntryStatusPollInterval)
 	
 });
